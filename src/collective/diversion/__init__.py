e1940d47075f8b04ea57c4abf89d53fb8e13104c
Matthew Wilkes <git@matthewwilkes.name>
2013-04-05 18:18:32 +0100

    Bind in the classFactory in Zope

--- src/collective/diversion/__init__.py
+++ src/collective/diversion/__init__.py
@@ -1,3 +1,5 @@
+from collective.diversion.diversion import rebind_ClassFactory
 
 def initialize(registrar):
-    pass
+    db = registrar._ProductContext__app._p_jar.db()
+    rebind_ClassFactory(db)
