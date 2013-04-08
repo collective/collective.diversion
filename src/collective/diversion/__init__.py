6e3e04e34d52de21715c648e69e16590b962a1ac
Matthew Wilkes <git@matthewwilkes.name>
2013-04-08 12:40:22 +0100

    Handle the fact that some things *cough*plone.testing*cough* wrap the ZODB in a weird facade before sending it in

--- src/collective/diversion/__init__.py
+++ src/collective/diversion/__init__.py
@@ -5,6 +5,12 @@ def initialize(registrar):
     # The ZMI keeps track of what ZODBs have been opened under what names for its Databases tab, grab those DB objects
     mounted_databases = getConfiguration().dbtab.databases
     for db in mounted_databases.values():
+        try:
+            # Some code wraps the DB in a facade before passing it to the dbtab
+            # They pass .open calls through, so use this to reclaim the original DB
+            db = db.open.im_self
+        except AttributeError:
+            pass
         # Switch out the function for resolving a class from an import location with our wrapper
         rebind_ClassFactory(db)
     
