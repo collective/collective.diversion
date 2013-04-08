2ee758de3f714f97b53c4cb70d1dddf5fcd326c4
Matthew Wilkes <git@matthewwilkes.name>
2013-04-08 12:39:57 +0100

    Use the App's DBTab for getting the mounted databases

--- src/collective/diversion/__init__.py
+++ src/collective/diversion/__init__.py
@@ -1,5 +1,10 @@
+from App.config import getConfiguration
 from collective.diversion.diversion import rebind_ClassFactory
 
 def initialize(registrar):
-    db = registrar._ProductContext__app._p_jar.db()
-    rebind_ClassFactory(db)
+    # The ZMI keeps track of what ZODBs have been opened under what names for its Databases tab, grab those DB objects
+    mounted_databases = getConfiguration().dbtab.databases
+    for db in mounted_databases.values():
+        # Switch out the function for resolving a class from an import location with our wrapper
+        rebind_ClassFactory(db)
+    
