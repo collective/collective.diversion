7693ae538eb622f78836067112aefb992d0bec84
Matthew Wilkes <git@matthewwilkes.name>
2013-04-08 13:06:52 +0100

    Add test for patching the classFactory

--- src/collective/diversion/tests/test_zcml.py
+++ src/collective/diversion/tests/test_zcml.py
@@ -51,6 +51,14 @@ class TestZCML(unittest.TestCase):
             del self.layer['configurationContext']
         
     
+    def test_loading_zcml_wraps_classFactory(self):
+        # We need the classFactory to be our one for this to actually do anything
+        # It is set up as a side-effect of installing the product. Hopefully this happens late enough for all other
+        # customisations to this setting to have been processed.
+        # 
+        # If there are problems on startup then you'll want to make this happen earlier, but I've not seen any failure
+        # modes like that
+        self.assertEqual(self.layer['zodbDB'].classFactory.__module__, 'collective.diversion.diversion')
 
 def test_suite():
     suite = unittest.TestSuite()
